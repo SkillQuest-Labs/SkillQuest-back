c45efa62132a828c1e22f012b4f0b2ace77c7612
johannes60 <garilsenan@gmail.com>
2025-06-12 22:18:06 +0200

    chore: refactor project strutur and integrate logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { LoggerModule } from 'nestjs-pino';
+import * as pino from 'pino';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // for loading environment variables from .env files
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule.forRootAsync({
+      useFactory: () => {
+        return {
+          pinoHttp: {
+            level: 'trace',
+            useLevelLabels: true,
+            transport:
+              process.env.NODE_ENV === 'local'
+                ? { target: 'pino-pretty' }
+                : undefined,
+            serializers: {
+              err: pino.stdSerializers.err,
+              req: pino.stdSerializers.req,
+              res: pino.stdSerializers.res,
+            },
+            redact: ['req.headers.authorization'],
+          },
+        };
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const logger = app.get(Logger);
+
+  app.useLogger(logger);
+
+  const packageBuffer = readFileSync('./package.json') as any;
+  const packageJson = JSON.parse(packageBuffer);
+  const version = packageJson.version;
+
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log(`Skill Quest API v${version} started`);
 }
 bootstrap();
